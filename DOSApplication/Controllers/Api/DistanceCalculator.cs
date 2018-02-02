f20a2e5479b721bdef3e9baa139cd3dcb568052e
Cristinel Mateias <webcr8@outlook.com>
2018-02-02 07:21:05 +0000

    mode fix

--- DOSApplication/Controllers/Api/DistanceCalculator.cs
+++ DOSApplication/Controllers/Api/DistanceCalculator.cs
@@ -23,7 +23,7 @@ namespace DOSApplication.Controllers.Api
                 using (var objClient = new WebClient())
                 {
                     var objReturn = new Dtl.DistanceMatrix();
-                    var strUrl = string.Format("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={0}&destinations={1}&mode={2}&key={3}", HttpUtility.UrlEncode(request.Origin), HttpUtility.UrlEncode(request.Destination), HttpUtility.UrlEncode(request.Mode), _key);
+                    var strUrl = string.Format("https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial&origins={0}&destinations={1}&mode={2}&key={3}", HttpUtility.UrlEncode(request.Origin), HttpUtility.UrlEncode(request.Destination), HttpUtility.UrlEncode(request.Mode.ToLower()), _key);
                     var result = objClient.DownloadString(strUrl);
                     objReturn = JsonConvert.DeserializeObject<Dtl.DistanceMatrix>(result);
 
